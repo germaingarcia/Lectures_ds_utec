3f528b3c57b66ff6ae396b289228f40873d92d40
germaingarcia <germaingarcia17@gmail.com>
2025-10-10 13:29:42 -0500

    Add files via upload

--- docs/script2.js
+++ docs/script2.js
@@ -28,73 +28,88 @@ var title_space ={'x':135, 'y':75,'w':1355,'h':25},
 var flag_opacity = false;
 var line_Group;
 
+//================================= UPDATE ONLY FROM THIS POINT ====================================
 var 
+CC1101= new Lecture(4,[],1,"CC1101","Cálculo de&una Variable","ciencias_generales",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC1141= new Lecture(2,[],1,"CC1141","Química&General","ciencias_generales",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+DS1142= new Lecture(3,[],1,"DS1142","Laboratorio de&Química& General","ciencias_generales",1,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 CS1111= new Lecture(1,[],1,"CS1111","&Programación I","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CC1101= new Lecture(2,[],1,"CC1101","Cálculo de&una Variable","ciencias_generales",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS1000= new Lecture(3,[],1,"DS1000","Introducción a&Ciencia&de Datos","ds",2,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS1021= new Lecture(4,[],1,"CS1021","&Matemáticas&Discretas I","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-HH1101= new Lecture(5,[],1,"HH1101","&Laboratorio de&Comunicación 1","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-PI1101= new Lecture(6,[],1,"PI1101","Proyectos&Inter-&disciplinarios 1","proyectos",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN1011= new Lecture(5,[],1,"EN1011","Introducción a&la Ingenería&de la Energía","en",2,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+HH1101= new Lecture(6,[],1,"HH1101","Laboratorio de&Comunicación 1","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+PI1101= new Lecture(7,[],1,"PI1101","Proyectos&Inter-&disciplinarios 1","proyectos",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 
-CS1112= new Lecture(2,[CS1111],2,"CS1112","&Programación II","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CC1104= new Lecture(1,[CC1101],2,"CC1104","&Cálculo&Vectorial","ciencias_generales",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CC1103= new Lecture(3,[CC1101],2,"CC1103","&Álgebra&Lineal","ciencias_generales",2,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CC1123= new Lecture(4,[CC1101],2,"CC1123","&Óptica y Ondas","ciencias_generales",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS1022= new Lecture(5,[CS1021],2,"CS1022","&Matemáticas&Discretas II","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-HH1102= new Lecture(6,[HH1101],2,"HH1102","&Laboratorio de&Comunicación 2","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC1103= new Lecture(3,[CC1101],2,"CC1103","Álgebra&Lineal","ciencias_generales",2,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC1104= new Lecture(2,[CC1101],2,"CC1104","Cálculo&Vectorial","ciencias_generales",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC1121= new Lecture(4,[CC1101],2,"CC1121","Introducción a&la Mecánica","ciencias_generales",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC1122= new Lecture(5,[CC1101],2,"CC1122","Fundamentos de&Electricidad&y Magnetismo","ciencias_generales",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC1112= new Lecture(1,[CS1111],2,"CC1112","&Programación II","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+HH1102= new Lecture(6,[HH1101],2,"HH1102","Laboratorio de&Comunicación 2","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 
-CC2101= new Lecture(4,[CC1104],3,"CC2101","&Ecuaciones&Diferenciales","ciencias_generales",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CC1105= new Lecture(2,[CC1101,CS1111],3,"CC1105","Estadística y&Probabilidades&I","ciencias_generales",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS2031= new Lecture(3,[CS1112],3,"CS2031","Desarrollo&Basado&en Plataformas","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS2013= new Lecture(1,[CS1112],3,"CS2013","&Programación&III","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS2041= new Lecture(6,[CS1112],3,"CS2041","&Base de&Datos I","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC1105= new Lecture(6,[CC1101,CS1111],3,"CC1105","Estadística y&Probabilidades&I","ciencias_generales",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC1143= new Lecture(3,[CC1141],3,"CC1143","Ciencia &de los Materiales","ciencias_generales",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC2101= new Lecture(2,[CC1104],3,"CC2101","Ecuaciones&Diferenciales","ciencias_generales",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC2121= new Lecture(1,[CC1121],3,"CC2121","&Termodinámica","ciencias_generales",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN2011= new Lecture(4   ,[CC1122],3,"EN2011","Ingeniería&eléctrica","en",5,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 PI2102= new Lecture(5,[PI1101],3,"PI2102","Proyectos&Inter-&disciplinarios 2","proyectos",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 
-CC2104= new Lecture(1,[CC1103,CC2101,CS1111],4,"CC2104","&Métodos&Numéricos","ciencias_generales",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS2023= new Lecture(2,[CS2013],4,"CS2023","Algoritmos y&Estructuras&de Datos","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CC2102= new Lecture(3,[CC1105],4,"CC2102","&Estadística y&Probabilidades II","ciencias_generales",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS2032= new Lecture(4,[CS2031],4,"CS2032","&Cloud&Computing","cs",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS2011= new Lecture(5,[CC1104,CS1112],4,"DS2011","&Optimización","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-GI2101= new Lecture(6,[],4,"GI2101","&Empresa&y Consumidor","gestion",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+CC2104= new Lecture(1,[CC1103,CC2101,CS1111],4,"CC2104","Métodos&Numéricos","ciencias_generales",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN2012= new Lecture(2,[CC2121],4,"EN2012","&Termofluidos","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN2013= new Lecture(3,[CC1103, EN2011],4,"EN2013","Máquinas&Eléctricas&","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN2014= new Lecture(4,[CC2121],4,"EN2014","Ingeniería&de Petróleo","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+GI2101= new Lecture(5,[],4,"GI2101","Perú: Temas&de la sociedad&contemporánea","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+HH2101= new Lecture(6,[],4,"HH2101","Empresa&y Consumidor","gestion",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 
-DS3012= new Lecture(1,[CS1022,CC2102],5,"DS3012","&Procesos&Estocásticos","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS3026= new Lecture(2,[CS2023],5,"CS3026","Análisis y&Diseño&de Algoritmos","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS3021= new Lecture(3,[CC1105,CS1112],5,"DS3021","Análisis&Computacional&de Datos","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS2042= new Lecture(6,[CS2041,CS2023],5,"CS2042","&Base de&Datos II","cs",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-HH2101= new Lecture(5,[],5,"HH2101","Perú: Temas&de la sociedad&contemporánea","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN3011= new Lecture(5,[EN2011],5,"EN3011","&Energía Eólica","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN3012= new Lecture(2,[EN2013],5,"EN3012","Sist. eléctricos&y electrónicos&de potencia","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN3013= new Lecture(6,[EN2011],5,"EN3013","&Hidroenergía","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN3014= new Lecture(3,[EN2012],5,"EN3014","Ingeniería de&los Gases&Sostenibles","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+HH2102= new Lecture(1,[],5,"HH2102","Eco. Gobernanza&y Relaciones&de Poder","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 PI3103= new Lecture(4,[PI2102],5,"PI3103","Proyectos&Inter-&disciplinarios 3","proyectos",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 
-CS3061= new Lecture(1,[CS1112,CC1105],6,"CS3061","&Machine&Learning","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS4052= new Lecture(2,[CS3026],6,"CS4052","Computación&Paralela&y Distribuida","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS3022= new Lecture(3,[DS3021,CS2032],6,"DS3022","Desarrollo de&Productos&de Datos","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS3031= new Lecture(4,[CS2042],6,"DS3031","Ética y&Seguridad&de los Datos","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-HH2102= new Lecture(5,[],6,"HH2102","Eco. Gobernanza&y Relaciones&de Poder","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-GI3101= new Lecture(6,[GI2101],6,"GI3101","&Finanzas&Empresariales","gestion",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN3015= new Lecture(1,[EN3013,CC2121],6,"EN3015","Energía Solar&y Termosolar","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN3016= new Lecture(2,[EN2012],6,"EN3016","Maquinas&Térmicas","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN3017= new Lecture(3,[EN2014,EN3013],6,"EN3017","Marco Legal y&Política&Energética","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN3018= new Lecture(4,[EN2013,EN3012],6,"EN3018","&Smart Grid	","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+ME3014= new Lecture(6,[EN2013],6,"ME3014","Automatización&de sistemas","me",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 
-DS4341= new Lecture(1,[CC1105,CS2041],7,"DS4341","&Big Data","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS4051= new Lecture(5,[DS3031],7,"DS4051","&Gobernanza&de Datos","ds",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS4061= new Lecture(3,[DS3022],7,"DS4061","&Storytelling&con Datos","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-CS5364= new Lecture(2,[CS3061],7,"CS5364","&Deep Learning","cs",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-EL= new Lecture(6,[],7,"EL","&Electivo&Libre 1","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN4011= new Lecture(1,[EN2013,EN3016],7,"EN4011","Auditoría y&Eficiencia&Energética","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN4012= new Lecture(5,[EN3015],7,"EN4012","Des. de proyectos&energéticos","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN4013= new Lecture(3,[],7,"EN4013","Ing. Ambiental &en el sector&energético","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN4014= new Lecture(2,[EN3013,EN3014],7,"EN4014","Planificación&y economía&de la Energía","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+GI3101= new Lecture(6,[HH2101],7,"GI3101","Finanzas&Empresariales","gestion",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+HH5101= new Lecture(7,[],7,"HH5101","Ética y&Tecnología","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 
-CS4091= new Lecture(5,[CS3026],8,"CS4091","Investigación&en&Computación","cs",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-PR4101= new Lecture(2,[],8,"PR4101","&Proyecto&Preprofesional","proyectos",8,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-HH3101= new Lecture(1,[],8,"HH3101","&Arte&y Tecnología","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+ELI= new Lecture(3,[],8,"ELI","Electivo&Libre 1","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+GI4101= new Lecture(2,[],8,"GI4101","Evaluación&Financiera&de Proyectos","gestion",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+PR4101= new Lecture(1,[],8,"PR4101","Proyecto&Preprofesional","proyectos",8,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 
-DS5044= new Lecture(2,[CS5364],9,"DS5044","Análisis &de Series&de Tiempo","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS5342= new Lecture(1,[DS4341],9,"DS5342","&Data Mining","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-HH5101= new Lecture(3,[],9,"HH5101","&Ética&y Tecnología","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-GI4101= new Lecture(4,[GI3101],9,"GI4101","Evaluación&Financiera&de Proyectos","gestion",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS5102= new Lecture(5,[CS4091],9,"DS5102","Proyecto Final&de Ciencia&de Datos I","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-ELII= new Lecture(6,[],9,"ELII","&Electivo&Libre 2","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+ELII= new Lecture(2,[],9,"ELII","Electivo&Libre 2","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+ELIII= new Lecture(1,[],9,"ELIII","Electivo&Libre 3","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+ELIV= new Lecture(3,[],9,"ELIV","Electivo&Libre 4","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN5101= new Lecture(4,[EN3018,ME3014],9,"EN5101","Proyecto Final&de Ingeniería&de la Energía I","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+GI5101= new Lecture(5,[GI4101],9,"GI5101","Estrategia y&Organizaciones","gestion",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+HH3101= new Lecture(6,[],9,"HH3101","Arte &y&Tecnología","humanidades",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
 
-DS5343= new Lecture(1,[DS5342],10,"DS5343","&Visualización&de Datos","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS5045= new Lecture(2,[CS5364],10,"DS5045","Ciencia&de Datos&Computacionales","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-GI5101= new Lecture(3,[GI4101],10,"GI5101","Estrategia&y&Organizaciones","gestion",3,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-DS5012= new Lecture(5,[DS5102],10,"DS5012","Proyecto Final&Ciencia&de Datos II","ds",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
-ELIII= new Lecture(6,[],10,"ELIII","&Electivo&Libre 3","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular");
+ELV= new Lecture(1,[],10,"ELV","Electivo&Libre 5","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+ELVI= new Lecture(2,[],10,"ELVI","Electivo&Libre 6","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+ELVII= new Lecture(3,[],10,"ELVII","Electivo&Libre 7","EL",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular"),
+EN5004= new Lecture(5,[EN5101],10,"EN5004","Proyecto Final&de Ingeniería&de la Energía II","en",4,"https://utec.edu.pe/carreras/ciencia-de-datos/malla-curricular")
 
-var all_lectures =[CS1111,CC1101,DS1000,CS1021,HH1101,PI1101,CS1112,CC1104,CC1103,CC1123,CS1022,HH1102,CC2101,CC1105,CS2031,CS2013,CS2041,PI2102,CC2104,CS2023,CC2102,CS2032,DS2011,GI2101,DS3012,CS3026,DS3021,CS2042,HH2101,PI3103,CS3061,CS4052,DS3022,DS3031,HH2102,GI3101,DS4341,DS4051,DS4061,CS5364,EL,CS4091,PR4101,HH3101,DS5044,DS5342,HH5101,GI4101,DS5102,ELII,DS5343,DS5045,GI5101,DS5012,ELIII];
+var all_lectures =[
+    CC1101, CC1141, DS1142, CS1111, EN1011, HH1101, PI1101,
+    CC1103, CC1104, CC1121, CC1122, CC1112, HH1102,
+    CC1105, CC1143, CC2101, CC2121, EN2011, PI2102,
+    CC2104, EN2012, EN2013, EN2014, GI2101, HH2101,
+    EN3011, EN3012, EN3013, EN3014, HH2102, PI3103,
+    EN3015, EN3016, EN3017, EN3018, ME3014,
+    EN4011, EN4012, EN4013, EN4014, GI3101, HH5101,
+    ELI, GI4101, PR4101,
+    ELII, ELIII, ELIV, EN5101, GI5101, HH3101,
+    ELV, ELVI, ELVII, EN5004
+];
+
+
+//============================================= UNTIL HERE =============================
 
 all_lectures.forEach(d=>{ all_lectures.forEach(e=>{  if ((e.pre).includes(d)) { d.add_pos(e);  }   }); });
 //First step is to put the titles
@@ -111,7 +126,7 @@ function redirect(evt) {
 
 var yRectScales = d3.scaleBand()
     .range([ rectangles_space.h, rectangles_space.y ])
-    .domain([6,5,4,3,2,1])
+    .domain([7,6,5,4,3,2,1])
     .padding(0.09);
 
 var gTitles = d3.select("#titles");
@@ -399,4 +414,5 @@ function show_Alternos_Pos(materia){
         d3.select("#"+'text_'+r.code).attr("fill-opacity", 1);
         show_Alternos_Pos(r);
     }
+
 }
